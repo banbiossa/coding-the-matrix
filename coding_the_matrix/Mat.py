ae3d5adfd27d474397af59e0303335f3b29be864
Shota Shimizu <shota.shimizu@quantumblack.com>
2021-02-05 09:16:52 +0900

    matrix

--- coding_the_matrix/Mat.py
+++ coding_the_matrix/Mat.py
@@ -1,4 +1,12 @@
+import pandas as pd
+
+
 class Mat:
     def __init__(self, labels, function):
         self.D = labels
         self.f = function
+
+    def __str__(self):
+        R, C = self.D
+        row_dict = {r: [self.f[(r, c)] for c in C] for r in R}
+        return pd.DataFrame.from_dict(row_dict, orient="index").to_string()
