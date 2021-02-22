82304dbd2a7afd236fa16c2388a897edd92e838d
Shota Shimizu <shota.shimizu@quantumblack.com>
2021-02-22 09:59:13 +0900

    stuff

--- coding_the_matrix/image_mat_util.py
+++ coding_the_matrix/image_mat_util.py
@@ -11,6 +11,59 @@ and we are in Python 3.9 with a rich ecosystem for dealing with png and arrays
 make a relatively simple API on our handling of pngs and arrays.
 """
 
+from coding_the_matrix import Vec
+from coding_the_matrix import Mat
+from coding_the_matrix.matutil import mat2coldict
+import matplotlib.pyplot as plt
+import io
+import numpy as np
+
+
+def show_colors(colors: Mat.Mat, locations: Mat.Mat):
+    """from a colors matrix and locations matrix, return the figure
+
+    Parameters
+    ----------
+    colors :
+    locations :
+
+    Returns
+    -------
+    figure
+
+    """
+    color_dict = mat2coldict(colors)
+    location_dict = mat2coldict(locations)
+
+    fig, ax = plt.subplots()
+    for top_left_point in sorted(color_dict):
+        # color
+        color = color_dict[top_left_point]
+        hex_color = rgb_to_hex(color)
+
+        # 4 points
+        corner_index = four_corners(*top_left_point)
+        corners = [location_dict[corner] for corner in corner_index]
+        x, y, u = corners_to_list(corners)
+        ax.fill(x, y, color=hex_color)
+    return fig
+
+
+def fig_to_array(fig) -> np.array:
+    """
+    A quick way to get the numpy representation from a fig.
+    Isn't complete, as this relies on how the figure is rendered.
+    """
+    fig.tight_layout(pad=0)
+    with io.BytesIO() as io_buf:
+        fig.savefig(io_buf, format="raw")
+        io_buf.seek(0)
+        img_arr = np.reshape(
+            np.frombuffer(io_buf.getvalue(), dtype=np.uint8),
+            newshape=(int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1),
+        )
+    return img_arr
+
 
 # Round color coordinate to nearest int and clamp to [0, 255]
 def _hex(n):
@@ -22,18 +75,22 @@ def _color_int(col: float) -> int:
     return max(min(round(col), 255), 0)
 
 
-def rgb_to_hex(rgb):
-    """3 value rgb tuple to hex"""
+def rgb_to_hex(rgb: Vec.Vec):
+    """3 value rgb Vec to hex"""
     assert len(rgb) == 3
-    rgb = [_color_int(color) for color in rgb]
-    return "#" + "".join([_hex(n) for n in rgb])
+    rgb_list = [_color_int(rgb[color]) for color in "rgb"]
+    return "#" + "".join([_hex(n) for n in rgb_list])
 
 
-def corners(x, y) -> list[list]:
+def four_corners(x, y) -> list[tuple]:
     """Return the corners, starting from `x, y` counter clockwise."""
     point_order = [(0, 0), (1, 0), (1, 1), (0, 1)]
-    corner_order = [(x + dx, y + dy) for dx, dy in point_order]
-    return [[corner[i] for corner in corner_order] for i in (0, 1)]
+    return [(x + dx, y + dy) for dx, dy in point_order]
+
+
+def corners_to_list(corners: list[Vec.Vec]):
+    """[Vecs] to [(x,...), (y,...)]"""
+    return [[corner[idx] for corner in corners] for idx in ["x", "y", "u"]]
 
 
 # utility conversions, between boxed pixel and flat pixel formats
--- tests/test_image_mat_util.py
+++ tests/test_image_mat_util.py
@@ -1,7 +1,14 @@
-from coding_the_matrix.image_mat_util import _color_int, corners
+from coding_the_matrix.image_mat_util import (
+    _color_int,
+    four_corners,
+    corners_to_list,
+    rgb_to_hex,
+)
 import pytest
 from coding_the_matrix.Mat import Mat
-from coding_the_matrix.matutil import listlist2mat
+from coding_the_matrix.Vec import Vec
+from coding_the_matrix.matutil import listlist2mat, mat2rowdict
+from coding_the_matrix.vecutil import list2vec
 import itertools
 
 
@@ -9,19 +16,19 @@ import itertools
 def gray_scale_squares():
     numbers = ["000111222", "012" * 3, "1" * 9]
     input_list = [[int(c) for c in number] for number in numbers]
-    location = listlist2mat(
+    locations = listlist2mat(
         input_list,
         rows=["x", "y", "u"],
         cols=[(x, y) for x, y in itertools.product(range(3), range(3))],
         # cols=[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)],
     )
-    color = listlist2mat(
+    colors = listlist2mat(
         # this becomes gray
         [[225, 125, 175, 75] for _ in range(3)],
         rows=["r", "g", "b"],
         cols=[(x, y) for x, y in itertools.product(range(2), range(2))],
     )
-    return location, color
+    return locations, colors
 
 
 @pytest.mark.parametrize("test_input,expected", [(3.2, 3), (-1, 0), (257, 255)])
@@ -33,22 +40,27 @@ def test_color_int(test_input, expected):
 @pytest.mark.parametrize(
     "test_input,expected",
     [
-        ([0, 0], [[0, 1, 1, 0], [0, 0, 1, 1]]),
-        (
-            [0, 1],
-            [
-                [0, 1, 1, 0],
-                [
-                    1,
-                    1,
-                    2,
-                    2,
-                ],
-            ],
-        ),
-        ([1, 0], [[1, 2, 2, 1], [0, 0, 1, 1]]),
+        ([0, 0], [(0, 0), (1, 0), (1, 1), (0, 1)]),
+        ([0, 1], [(0, 1), (1, 1), (1, 2), (0, 2)]),
+        ([1, 0], [(1, 0), (2, 0), (2, 1), (1, 1)]),
     ],
 )
-def test_corners(test_input, expected):
-    actual = corners(*test_input)
+def test_four_corners(test_input, expected):
+    actual = four_corners(*test_input)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected", [([0, 0, 0], "#000000"), ([0, 255, 0], "#00ff00")]
+)
+def test_rgb_to_hex(test_input, expected):
+    color = Vec({"r", "g", "b"}, {c: test_input[i] for i, c in enumerate("rgb")})
+    actual = rgb_to_hex(color)
+    assert actual == expected
+
+
+def test_corners_to_list():
+    corners = mat2rowdict(listlist2mat([[0, 1, 1], [1, 0, 1]], cols=["x", "y", "u"]))
+    actual = corners_to_list(corners.values())
+    expected = [[0, 1], [1, 0], [1, 1]]
     assert actual == expected
